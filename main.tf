64250bc57af50775d68081424ec67ecf7e670703
sandeepnaidu <nainala_sandeep@yahoo.com>
2023-07-19 17:50:10 +0530

    aws ssm parameter store- terraform

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 resource "aws_ssm_parameter" "parameter" {
-  name  = "test.test1"
-  type  = "String"
-  value = "Hello World"
+  name   = "test.test1"
+  type   = "SecureString"
+  value  = "Hello World"
+  key_id = "91f66eef-725c-44da-8b67-33f53d25404f"
 }
