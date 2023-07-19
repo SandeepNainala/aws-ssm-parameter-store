18fda04078e202f7f9eee7dd8c4aaf507c800fc6
sandeepnaidu <nainala_sandeep@yahoo.com>
2023-07-19 18:22:34 +0530

    aws ssm parameter store- terraform roboshop

---
+++ env-dev/main.tfvars
@@ -0,0 +1,4 @@
+parameters = {
+  {name = "dev.frontend.catalogue_url", value = "http://catalogue-dev.devops71.cloud:8080"},
+  {name = "dev.cart.catalogue_url", value = "http://cart-dev.devops71.cloud:8080"}
+}
--- main.tf
+++ main.tf
@@ -1,6 +1,8 @@
-resource "aws_ssm_parameter" "parameter" {
-  name   = "test.test1"
-  type   = "SecureString"
-  value  = "Hello World"
-  key_id = "91f66eef-725c-44da-8b67-33f53d25404f"
+resource "aws_ssm_parameter" "parameters" {
+  count   = length(var.parameters)
+  name    = var.parameters[count.index].name
+  type    = "String"
+  value   = var.parameters[count.index].value
+
+  key_id  = "91f66eef-725c-44da-8b67-33f53d25404f"
 }
